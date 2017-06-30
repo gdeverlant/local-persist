06869ed9a7d73ca3bf36019980842aafb3a86195
ergü <ergu@erguder.tk>
2017-06-30 14:32:40 +0200

    Added arm32/arm64 installation and compilation.

--- Makefile
+++ Makefile
@@ -29,32 +29,35 @@ binary:
 	go build -o bin/$(BIN_NAME) -v
 
 # build all the binaries
-binaries: clean-bin binary-linux-amd64 # binary-linux-386 binary-linux-arm binary-freebsd-amd64 binary-freebsd-386
+binaries: clean-bin binary-linux-amd64 binary-linux-386 binary-linux-arm binary-linux-arm64 #binary-freebsd-amd64 binary-freebsd-386
 
 clean-bin:
 	rm -Rf bin/*
 
-# build a specific binary
 binary-linux-amd64: export GOOS=linux
 binary-linux-amd64: export GOARCH=amd64
 binary-linux-amd64: export GO15VENDOREXPERIMENT=1
 binary-linux-amd64:
 	go build -o bin/$(GOOS)/$(GOARCH)/$(BIN_NAME) -v
 
-# docker doesn't currently support x32 architecture
-# binary-linux-386: export GOOS=linux
-# binary-linux-386: export GOARCH=386
-# binary-linux-386: export GO15VENDOREXPERIMENT=1
-# binary-linux-386:
-# 	go build -o bin/$(GOOS)/$(GOARCH)/$(BIN_NAME) -v
+binary-linux-386: export GOOS=linux
+binary-linux-386: export GOARCH=386
+binary-linux-386: export GO15VENDOREXPERIMENT=1
+binary-linux-386:
+ 	go build -o bin/$(GOOS)/$(GOARCH)/$(BIN_NAME) -v
 
-# docker doesn't currently support arm architecture
-# binary-linux-arm: export GOOS=linux
-# binary-linux-arm: export GOARCH=arm
-# binary-linux-arm: export GO15VENDOREXPERIMENT=1
-# binary-linux-arm:
-# 	go build -o bin/$(GOOS)/$(GOARCH)/$(BIN_NAME) -v
+binary-linux-arm: export GOOS=linux
+binary-linux-arm: export GOARCH=arm
+binary-linux-arm: export GO15VENDOREXPERIMENT=1
+binary-linux-arm:
+ 	go build -o bin/$(GOOS)/$(GOARCH)/$(BIN_NAME) -v
 
+binary-linux-arm64: export GOOS=linux
+binary-linux-arm64: export GOARCH=arm64
+binary-linux-arm64: export GO15VENDOREXPERIMENT=1
+binary-linux-arm64:
+ 	go build -o bin/$(GOOS)/$(GOARCH)/$(BIN_NAME) -v
+	
 # cowardly unwilling to support other architectures for now
 # binary-freebsd-amd64: export GOOS=freebsd
 # binary-freebsd-amd64: export GOARCH=amd64
--- scripts/install.sh
+++ scripts/install.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-VERSION="v1.3.0"
+VERSION="v1.3.1"
 
 # uname -s, uname -m
 # Deb 32: Linux i686
@@ -27,13 +27,18 @@ function setenv {
                 SUPPORTED=true
             ;;
             "i686")
-                # ARCH="386"
-                SUPPORTED=false
+                ARCH="386"
+                SUPPORTED=true
             ;;
             # untested
             arm*)
-                # ARCH="arm"
-                SUPPORTED=false
+                ARCH="arm32"
+                SUPPORTED=true
+            ;;
+			# untested
+            aarch64*)
+                ARCH="arm64"
+                SUPPORTED=true
             ;;
         esac
     elif [[ $OS == 'freebsd' ]]; then
@@ -56,7 +61,7 @@ function install-binary {
         (sudo systemctl stop docker-volume-local-persist || true)
     fi
 
-    BINARY_URL="https://github.com/CWSpear/local-persist/releases/download/${VERSION}/local-persist-${OS}-${ARCH}"
+	BINARY_URL="https://github.com/gdeverlant/local-persist/releases/download/${VERSION}/local-persist-${OS}-${ARCH}"
     BINARY_DEST="/usr/bin/docker-volume-local-persist"
 
     echo Downloading binary:
@@ -72,7 +77,8 @@ function install-binary {
 
 # Systemd (default)
 function setup-systemd {
-    SYSTEMD_CONFIG_URL="https://raw.githubusercontent.com/CWSpear/local-persist/${VERSION}/init/systemd.service"
+	SYSTEMD_CONFIG_URL="https://raw.githubusercontent.com/gdeverlant/local-persist/master/init/systemd.service"
+	
     SYSTEMD_CONFIG_DEST="/etc/systemd/system/docker-volume-local-persist.service"
 
     echo Downloading Systemd service conf:
@@ -99,7 +105,7 @@ function start-systemd {
 
 # Upstart
 function setup-upstart {
-    UPSTART_CONFIG_URL="https://raw.githubusercontent.com/CWSpear/local-persist/${VERSION}/init/upstart.conf"
+	UPSTART_CONFIG_URL="https://raw.githubusercontent.com/gdeverlant/local-persist/master/init/upstart.conf"
     UPSTART_CONFIG_DEST="/etc/init/docker-volume-local-persist.conf"
 
     echo Downloading binary:
